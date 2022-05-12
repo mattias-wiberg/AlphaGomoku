9380226614489482abbb4bd21ca54a37dffad45d
alexeygorskiy <alexey.gorskiy@gmail.com>
2022-05-12 14:40:48 +0200

    added n_moves

--- gameboard.py
+++ gameboard.py
@@ -17,6 +17,7 @@ class GameBoard:
         self.black_move_history = []
         self.white_to_play_history = []
         self.white_move_history = []
+        self.n_moves = 0
 
     def restart(self):
         self.board = np.zeros((self.N_row, self.N_col), dtype=np.int8)
@@ -26,6 +27,7 @@ class GameBoard:
         self.black_move_history = []
         self.white_to_play_history = []
         self.white_move_history = []
+        self.n_moves = 0
 
     def get_seq_reward(self, sequence):
         consecutive_seq_sum = np.sum(sequence[0:5])
@@ -83,19 +85,23 @@ class GameBoard:
         
         if self.piece == -1:
             self.black_to_play_history.append(copy.deepcopy(self.board))
-            self.black_move_history.append(copy.deepcopy((row, col)))
+            self.black_move_history.append((row, col))
             if len(self.black_to_play_history) == 3:
                 self.black_to_play_history.pop(0)
                 self.black_move_history.pop(0)
         elif self.piece == 1:
             self.white_to_play_history.append(copy.deepcopy(self.board))
-            self.white_move_history.append(copy.deepcopy((row, col)))
+            self.white_move_history.append((row, col))
             if len(self.white_to_play_history) == 3:
                 self.white_to_play_history.pop(0)
                 self.white_move_history.pop(0)
 
         self.board[row,col] = self.piece
-        reward = self.get_reward(row, col)
+        self.n_moves += 1
+        if self.n_moves >= 9:
+            reward = self.get_reward(row, col)
+        else:
+            reward = 0
         if reward != 0:
             self.gameover = True
             self.piece *= -1
