2526947341cd2d0ea085beb9806e23af673c0b7a
Henrik Grönbäck <henrik.gronback@hotmail.com>
2022-05-17 11:54:58 +0200

    added human playing vs machine

--- main.py
+++ main.py
@@ -1,20 +1,47 @@
 from gameboard import GameBoard
 from agent import TDQNAgent
 
-visualize = False
-strategy_file = ''
+visualize = True
+strategy_file = 'qn_mean_13_2.pth'
+
+human = 1
+human_start_piece = -1
 
 gameboard = GameBoard(15, 15)
+
 if strategy_file:
     agent = TDQNAgent(gameboard, episode_count=50000, epsilon_scale=0)
 else:
     agent = TDQNAgent(gameboard=gameboard)
+
 if strategy_file:
     agent.load_strategy(strategy_file)
-    while True:
-        agent.turn()
-        if visualize:
-            gameboard.plot()
+    if human:
+        if human_start_piece == -1:
+            while True:
+                agent.turn()
+                if visualize:
+                    gameboard.plot()
+                x, y = [int(x) for x in input().split()]
+                agent.turn(forced_move=[x,y])
+                if visualize:
+                    gameboard.plot()
+
+        if human_start_piece == 1:
+            while True:
+                x, y = [int(x) for x in input().split()]
+                agent.turn(forced_move=[x,y])
+                if visualize:
+                    gameboard.plot()
+                agent.turn()
+                if visualize:
+                    gameboard.plot()
+    
+    else:
+        while True:
+            agent.turn()
+            if visualize:
+                gameboard.plot()
 else:
     while True:
         agent.turn()
