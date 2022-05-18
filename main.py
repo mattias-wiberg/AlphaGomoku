ac06d07e57f04aea54282f9b3944275f56be95f0
alexeygorskiy <alexey.gorskiy@gmail.com>
2022-05-18 14:01:57 +0200

    fixed human player order

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 from gameboard import GameBoard
 from agent import TDQNAgent
 
-visualize = True
-strategy_file = 'qn_mean_13_2.pth'
+visualize = False
+strategy_file = ''
 
-human = 1
-human_start_piece = -1
+human = 0
+human_start_piece = 1
 
 gameboard = GameBoard(15, 15)
-
-if strategy_file:
-    agent = TDQNAgent(gameboard, episode_count=50000, epsilon_scale=0)
-else:
-    agent = TDQNAgent(gameboard=gameboard)
+agent = TDQNAgent(gameboard)
 
 if strategy_file:
     agent.load_strategy(strategy_file)
     if human:
-        if human_start_piece == -1:
+        if human_start_piece == 1:
             while True:
                 agent.turn()
                 if visualize:
@@ -27,7 +23,7 @@ if strategy_file:
                 if visualize:
                     gameboard.plot()
 
-        if human_start_piece == 1:
+        if human_start_piece == -1:
             while True:
                 x, y = [int(x) for x in input().split()]
                 agent.turn(forced_move=[x,y])
